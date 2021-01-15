b5a8d9bccd2152cb3207ed04b56b7c5b26dccfbf
Ravichanderreddy-goli <ravichanderreddy73@gmail.com>
2021-01-14 21:56:08 -0600

    help

--- app.js
+++ app.js
@@ -15,6 +15,9 @@ app.get('/contact',(req,res)=>{
 app.get('/help',(req,res)=>{
     res.send('Help page')
 })
+app.get('/help/ravi',(req,res)=>{
+    res.send('ravi is there for help')
+})
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
