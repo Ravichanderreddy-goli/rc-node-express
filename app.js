1f458a1bd8576309e3df79bb99010683f5de2cf3
Ravichanderreddy-goli <ravichanderreddy73@gmail.com>
2021-01-14 19:45:58 -0600

    modify

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000 
+const port = process.env.PORT || 3000
+
+app.get('/', (req,res) => {
+    res.send('Hello World - Ravi App!')
+})
 
 app.get('/about', (req, res) => {
   res.send("About ravi page.")
